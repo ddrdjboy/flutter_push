6fbae099638e415c9409c52b0858e1ed9e1759b9
weigangchen <isamanthena@gmail.com>
2026-04-02 11:13:13 +0800

    888

--- src/main/java/com/isamanthena/push/controller/DeviceController.java
+++ src/main/java/com/isamanthena/push/controller/DeviceController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class DeviceController {
 // 7
+// 8
     private final DeviceService deviceService;
 
     @PostMapping("/register")
